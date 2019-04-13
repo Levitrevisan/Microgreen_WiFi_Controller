4e11ff2cc86f9a5dfd2cfa73ef1ef3881d9e2232
Levi Trevisan <levitrevisan@hotmail.com>
2019-04-13 14:28:48 -0300

    Added WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN,OUTPUT);
+const char *ssid = "Trevisan";
+const char *password = "99194213";
+
+void setup()
+{
+  // Start serial connection
+  Serial.begin(9600);
+  // Start IO ports
+  pinMode(LED_BUILTIN, OUTPUT);
+  //configure WiFi Connection
+  Serial.println("");
+  Serial.print("Trying to connect to: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected with ip: ");
+  Serial.print(WiFi.localIP());
+  Serial.println("");
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN,HIGH);
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
-  digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
   // put your main code here, to run repeatedly:
 }
